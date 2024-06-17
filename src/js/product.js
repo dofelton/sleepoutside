fafed86a2788c92f1e219ec7e179b612e23d714c
Valeria Esquivel <esq22001@byui.edu>
2024-06-17 17:42:41 -0600

    Items added to cart in array for more than one product

--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,30 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
-function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+// let listCart = getLocalStorage("so-cart");
+// recuperar en forma de array
+let listCart = [];
+if (getLocalStorage("so-cart")) {
+  listCart = getLocalStorage("so-cart");
 }
+
+function addProductToCart(product) {
+  if (listCart.length > 0) {
+    const productInCart = listCart.find((p) => p.Id === product.Id);
+    if (productInCart) {
+      alert("Item already in cart!");
+
+      return;
+    }
+  }
+
+  listCart.push(product);
+
+  setLocalStorage("so-cart", listCart);
+}
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
