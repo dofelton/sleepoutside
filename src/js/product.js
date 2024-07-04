a0ee272849e8c6b06af708cd6ccdcb6adde634ff
dofelton <dofelton@gmail.com>
2024-07-04 11:45:11 -0500

    Start week4

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,10 @@
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
-import { getParams, loadHeaderFooter } from "./utils.mjs";
+import { getParam, loadHeaderFooter } from "./utils.mjs";
 
 loadHeaderFooter();
 
-const productId = getParams("product");
+const productId = getParam("product");
 const dataSource = new ProductData("tents");
 
 const product = new ProductDetails(productId, dataSource);
